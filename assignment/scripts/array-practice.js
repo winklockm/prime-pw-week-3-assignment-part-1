cf63f6aabb05a9073e5a85235cc23c54396149b5
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-25 12:43:24 -0500

    console logged number of foods

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -22,7 +22,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-
+console.log('Number of foods:', foodArray.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
