056becddc98311f3fd92f383743eb171f5edb3e2
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-27 18:05:20 -0500

    converted array to a string connected by ands

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -95,6 +95,8 @@ console.log('My favorite foods in reverse alphabetical order are', foodArray.rev
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+let string = [foodArray[0],foodArray[1],foodArray[2],foodArray[3],].join(' and ')
+console.log(string);
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
